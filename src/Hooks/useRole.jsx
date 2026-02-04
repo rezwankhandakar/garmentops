dac33f70bf4e937b644fc84009a32a7a44c03896
rezwankhandakar <rezwankhandaker02@gmail.com>
2026-02-04 23:01:13 +0600

    manage users

---
+++ src/Hooks/useRole.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+
+const useRole = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { isLoading, data } = useQuery({
+    queryKey: ['user-role', user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/user/${user.email}/role`);
+      return res.data;
+    }
+  });
+
+  return {
+    role: data?.role || 'user',
+    isLoading
+  }
+};
+
+export default useRole;
